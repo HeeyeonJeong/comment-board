dedfc67d9b607843e877931a804c52a408989fe9
HeeyeonJeong <junghy14971@gmail.com>
2021-03-16 16:05:33 +0900

    Fix: remove logger form deploy

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import modules from "./modules";
 import ReduxThunk from "redux-thunk";
 import { createLogger } from "redux-logger";
@@ -6,10 +6,12 @@ import { composeWithDevTools } from "redux-devtools-extension";
 
 const logger = createLogger();
 
+const enhancer =
+  process.env.NODE_ENV === "production"
+    ? compose(applyMiddleware(ReduxThunk))
+    : composeWithDevTools(applyMiddleware(ReduxThunk, logger));
+
 export default function configureStore() {
-    const store = createStore(
-        modules /* preloadedState, */,
-        composeWithDevTools(applyMiddleware(ReduxThunk, logger))
-    );
-    return store;
+  const store = createStore(modules, enhancer);
+  return store;
 }
