1dc99bc73065bbcdb607de81ee7daf53a43a63c4
HeeyeonJeong <junghy14971@gmail.com>
2021-03-16 18:17:12 +0900

    Fix: change json api

--- src/api/comments.js
+++ src/api/comments.js
@@ -1,41 +1,45 @@
 import axios from "axios";
 
 export const getComments = async () => {
-    const response = await axios.get("http://localhost:4000/comments");
-    return response.data;
+  const response = await axios.get(
+    "https://comments-board.herokuapp.com/api/comments"
+  );
+  return response.data;
 };
 
 export const getComment = async (id) => {
-    const response = await axios.get(`http://localhost:4000/comments/${id}`);
-    return response.data;
+  const response = await axios.get(
+    `https://comments-board.herokuapp.com/api/comments/${id}`
+  );
+  return response.data;
 };
 
 export const pageComments = async (num) => {
-    const reponse = await axios.get(
-        `http://localhost:4000/comments?_page=${num}&_limit=3&_order=desc&_sort=id`
-    );
-    return reponse.data;
+  const reponse = await axios.get(
+    `https://comments-board.herokuapp.com/api/comments?_page=${num}&_limit=3&_order=desc&_sort=id`
+  );
+  return reponse.data;
 };
 
 export const addComment = async (comment) => {
-    const response = await axios.post(
-        "http://localhost:4000/comments",
-        comment
-    );
-    return response.data;
+  const response = await axios.post(
+    "https://comments-board.herokuapp.com/api/comments",
+    comment
+  );
+  return response.data;
 };
 
 export const editComment = async (comment) => {
-    const response = await axios.put(
-        `http://localhost:4000/comments/${comment.id}`,
-        comment
-    );
-    return response.data;
+  const response = await axios.put(
+    `https://comments-board.herokuapp.com/api/comments/${comment.id}`,
+    comment
+  );
+  return response.data;
 };
 
 export const removeComment = async (comment) => {
-    const response = await axios.delete(
-        `http://localhost:4000/comments/${comment.id}`
-    );
-    return response.data;
+  const response = await axios.delete(
+    `https://comments-board.herokuapp.com/api/comments/${comment.id}`
+  );
+  return response.data;
 };
